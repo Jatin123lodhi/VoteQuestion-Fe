c13bb4a9499f32c7b30ca3cc29583d30dbad10b6
Jatin Lodhi <jatinlodhi111@gmail.com>
2023-10-04 18:13:15 +0530

    server url changed

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-export const SERVER_BASE_URL = "http://localhost:3000";
+export const SERVER_BASE_URL = "https://votequesitonbea.onrender.com";
 export const AUTH_HEADER = {
   Authorization: "Bearer " + localStorage.getItem("token"),
 };
